fe38d634b19671521efb88aadf975a8fa6fd97db
sarkatesiddhant <sarkatesiddhant@gmail.com>
2026-01-29 23:37:49 +0530

    something

--- StarField/starfield.c
+++ StarField/starfield.c
@@ -1,7 +1,11 @@
-//#include <SDL2/SDL.h>
+#include <SDL3/SDL.h>
 #include <stdio.h>
 
 int main()
 {
+    SDL_Init(SDL_INIT_VIDEO);
     printf("Hello, Space, this is GOD!\n");
+    SDL_Window *pwindow = SDL_CreateWindow("Starfield", 50, 400, 0);
+
+    SDL_Delay(5000);
 }
